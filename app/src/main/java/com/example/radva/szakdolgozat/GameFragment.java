9318f90d794f0c9a6eed83487d6e53ff45e0d699
Radványi Tamás <radvanyi.tamas910105@gmail.com>
2018-05-17 12:44:01 +0200

    Game #2 isActived Work and added Game mezo### id

--- app/src/main/java/com/example/radva/szakdolgozat/GameFragment.java
+++ app/src/main/java/com/example/radva/szakdolgozat/GameFragment.java
@@ -1,15 +1,23 @@
 package com.example.radva.szakdolgozat;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.graphics.ColorSpace;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.shapes.Shape;
+import android.os.Build;
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class GameFragment extends Fragment {
@@ -23,16 +31,34 @@ public class GameFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_game, container, false);
         // Inflate the layout for this fragment
-        final Button mezo1 = container.findViewById(R.id.mezo001);
+        final ImageView mezo1 = view.findViewById(R.id.mezo001);
+        final ImageView mezo2 = view.findViewById(R.id.mezo002);
         mezo1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mezo1.setVisibility(View.GONE);
+                if (!mezo1.isActivated()) {
+                    mezo1.setActivated(true);
+                } else {
+                    mezo1.setActivated(false);
+                }
             }
         });
 
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        mezo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mezo2.isActivated()) {
+                    mezo2.setActivated(true);
+                } else {
+                    mezo2.setActivated(false);
+                }
+            }
+        });
+
+        return view;
+
     }
 
 
